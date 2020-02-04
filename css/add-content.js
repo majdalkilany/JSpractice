22c98dfab2602c3f24e2a2ba266c25b4574b0444
majdalkilany <majdkilany922@gmail.com>
2020-02-04 21:17:37 +0200

    repeting the code

--- css/add-content.js
+++ css/add-content.js
@@ -1,12 +1,18 @@
 var today = new Date();
 var hourNow = today.getUTCHours;
 var greetng;
+var today= new Date();
+var hourNow = today.getHours();
 
-if (hourNow> 18) {
-    greetng = 'good evening!';
-} else if (hourNow > 12) {greetng ='goodafternoon!';}
+var greeting;
+if (hourNow > 18) {
+greeting= 'Good evening!';
+else if (hourNow > 12) {
+greeting = ' Good afternoon!';
+
 else if (hourNow > 0) {
-    greeting = 'Good morni ng!';
-    else {
-    greeting = 'Welcome! ' ;
-    document .write( ' <h3>' +greeting + ' </ h3> '); 
+greeting = 'Good morni ng!';
+
+else {
+greeting = 'Welcome! ' ;
+document .write( ' <h3>' +greeting + ' </ h3> '); 
