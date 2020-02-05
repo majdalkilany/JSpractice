41aa17ba2c46100ae5b5046859fa4f4e31c66ccd
majdalkilany <majdkilany922@gmail.com>
2020-02-05 09:28:06 +0200

    coardanait code

--- css/add-content.js
+++ css/add-content.js
@@ -1,9 +1,14 @@
 var today = new Date();
 
 var hourNow = today.getHours();
+
 var greeting; 
 if (hourNow > 18) {greeting = 'Good evening!';} 
+
 else if (hourNow > 12) {greeting = 'Good afternoon!';}
-else if (hourNow > 0) {greeting = 'Good morni ng!';}
+
+else if (hourNow > 0) {greeting = 'Good morning!';}
+
 else {greeting = 'Welcome!';}
+
 document .write ( ' <h3>' +greeting + ' </ h3> ');
